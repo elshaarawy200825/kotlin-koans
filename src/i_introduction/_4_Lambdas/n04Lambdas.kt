848b7d956cb9d2473c24f43e8819aa8ed1eb720a
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2018-06-01 06:35:55 +0200

    feat: solve lambdas

--- src/i_introduction/_4_Lambdas/n04Lambdas.kt
+++ src/i_introduction/_4_Lambdas/n04Lambdas.kt
@@ -2,6 +2,8 @@ package i_introduction._4_Lambdas
 
 import util.TODO
 import util.doc4
+import java.util.function.Consumer
+import java.util.function.Predicate
 
 fun example() {
 
@@ -12,14 +14,14 @@ fun example() {
 }
 
 fun todoTask4(collection: Collection<Int>): Nothing = TODO(
-    """
+        """
         Task 4.
         Rewrite 'JavaCode4.task4()' in Kotlin using lambdas:
         return true if the collection contains an even number.
         You can find the appropriate function to call on 'Collection' by using code completion.
         Don't use the class 'Iterables'.
     """,
-    documentation = doc4(),
-    references = { JavaCode4().task4(collection) })
+        documentation = doc4(),
+        references = { JavaCode4().task4(collection) })
 
-fun task4(collection: Collection<Int>): Boolean = todoTask4(collection)
+fun task4(collection: Collection<Int>): Boolean = collection.any({ int -> int % 2 == 0 })
